06d6fd92eb4f33d97026dd82991b183dad49bd6a
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-12 22:21:33 -0300

    feat: cria o controller e os primeiros endPoints getAllCategories e saveCategory

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,4 +1,27 @@
 package com.movieflix.controller;
 
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping()
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/create")
+    public Category saveCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
+
 }
