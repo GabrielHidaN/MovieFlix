f1382d2fce283188532322027f4cb7dd8a08ee2c
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-12 22:23:20 -0300

    feat: cria os services de getCategories e saveCategory

--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -1,4 +1,25 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
+import com.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category saveCategory(Category category){
+        return categoryRepository.save(category);
+    }
+
+
 }
